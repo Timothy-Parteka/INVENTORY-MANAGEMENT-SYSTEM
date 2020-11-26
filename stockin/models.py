9ee781fa52d283d4110a1ee4aad82c21b36fb950
Timothy-Parteka <39987418+Timothy-Parteka@users.noreply.github.com>
2020-11-26 04:19:44 +0300

    Update models.py

--- stockin/models.py
+++ stockin/models.py
@@ -4,12 +4,19 @@ from django.contrib.auth import get_user_model
 
 
 class StockIn(models.Model):
+    
+    ITEM_RECEIVED = (
+
+    ('YES', 'yes'),
+    ('NO', 'no'),
+
+    )
     item = models.CharField(max_length=100)
     item_ref_no = models.CharField(max_length=255,null=True, blank=True)
     count = models.CharField(max_length=100)
     date_purchased = models.DateTimeField(default=now)
     business_name = models.CharField(max_length=255, null=True, blank=True)
-    received = models.BooleanField(default=False)
+    item_received = models.CharField(max_length=255, choices=ITEM_RECEIVED, null=True, verbose_name='item received')
     date_received = models.DateTimeField(default=now)
     received_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
